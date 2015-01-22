2f7e0a32f1909e4501646ceb04bdc80ad18f1843
SimonLab <simon.labondance@gmail.com>
2015-01-22 18:28:38 +0000

    Simple change of action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render text: "Salut Simon"
+    render text: "Hello there"
+  end
+
+  def goodbye
+    render text: "See you later"
   end
 end
